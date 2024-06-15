7a9aef07ed5bf4f132f7dea898036b847b7cec18
Cooper Corbett <coopercorbett@gmail.com>
2024-06-15 21:22:20 +1000

    💚 Turn off webpack minification

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.optimization.minimize = false;
+    config.optimization.minimizer = [];
+    return config;
+  },
+};
 
 export default nextConfig;
